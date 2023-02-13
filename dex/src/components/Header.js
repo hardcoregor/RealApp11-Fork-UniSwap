1b1a2d998f43a6047ac92420ee4e642c356849c1
hardcoregor <spraytown2014@gmail.com>
2023-02-13 21:19:41 +0200

    swap frontend done

--- dex/src/components/Header.js
+++ dex/src/components/Header.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '../moralis-logo.svg';
+import Eth from '../eth.svg';
+
+const connect = () => {
+}
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='leftH'>
+        <img src={Logo} className="logo" alt="logo" />
+        <Link to="/" className='link'>
+          <div className='headerItem'>Swap</div>
+        </Link>
+        <Link to="/tokens" className='link'>
+          <div className='headerItem'>Tokens</div>
+        </Link>
+      </div>
+      <div className='rightH'>
+        <div className='headerItem'>
+          <img src={Eth} alt="eth" className='eth' />
+          Ethereum
+        </div>
+        <div className='connectButton' onClick={connect}>
+          Connect
+        </div>
+      </div>
+    </header>
   )
 }
 
